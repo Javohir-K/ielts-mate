ba76ca5cff003bb0ebb81c0a9df36fdc01fc7499
JAVOHIR KOMILJONOV <61010500+Javohir-K@users.noreply.github.com>
2023-07-07 23:24:20 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCH2ERieoEKC64xM_hiBn9rkoyYDcq7JLc",
-  authDomain: "ielts-mate.firebaseapp.com",
-  projectId: "ielts-mate",
-  storageBucket: "ielts-mate.appspot.com",
-  messagingSenderId: "749422146569",
-  appId: "1:749422146569:web:25bc353848adfc03ab74d8",
-  measurementId: "G-NCD8P89K2K",
+  // firebase config data here. I have deleted it for security purposes
 };
 const app = firebase.initializeApp(firebaseConfig);
 const db = app.firestore();
